36a6bbcaa88e7d86ed417868f9a350efec62fe15
Sergh <serghbr@gmail.com>
2022-03-20 22:21:02 +0300

    Добавил приветствие, подправил отступы

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,9 @@ public class Main {
         long rez;
         Check test = new Check();
         java.util.Scanner in = new Scanner(System.in);
+        System.out.println("Добро пожаловать в приложение калькулятор\nКалькулятор работает с арабскими и римскими числами\n" +
+                "Арабские числа и результат их вычислений и должны укладываться в диапазон -2,147,483,648 до 2,147,483,647\n" +
+                "Римские числа и результат их вычислений и должны быть больше нуля и не более MMM (3,000)");
 
         while (true) {
 
@@ -29,19 +32,19 @@ public class Main {
             }
 
             else if (test.getResult().equals("int")) {
-                    rez = calc(Integer.parseInt(strCheck[0]), strCheck[1], Integer.parseInt(strCheck[2]));
-                    if (Integer.MIN_VALUE < rez && rez < Integer.MAX_VALUE) {
-                        System.out.println(rez);
-                    }
-                    else throw new RuntimeException("Результат вычеслений должен лежать в диапазоне от -2,147,483,648 до 2,147,483,647");
+                rez = calc(Integer.parseInt(strCheck[0]), strCheck[1], Integer.parseInt(strCheck[2]));
+                if (Integer.MIN_VALUE < rez && rez < Integer.MAX_VALUE) {
+                    System.out.println(rez);
+                }
+                else throw new RuntimeException("Результат вычеслений должен лежать в диапазоне от -2,147,483,648 до 2,147,483,647");
             }
 
             else if (test.getResult().equals("rim")) {
-                    rez = calc (Convert.convert(strCheck[0]), strCheck[1], Convert.convert(strCheck[2]));
-                    if (rez > 0 && rez <= 3000) {
-                        System.out.println(Convert.convert(rez));
-                    }
-                    else throw new RuntimeException("Для римских чисел результат вычеслений не может быть отрицательным и не должен привышать MMM(3000)");
+                rez = calc (Convert.convert(strCheck[0]), strCheck[1], Convert.convert(strCheck[2]));
+                if (rez > 0 && rez <= 3000) {
+                    System.out.println(Convert.convert(rez));
+                }
+                else throw new RuntimeException("Для римских чисел результат вычеслений не может быть отрицательным и не должен привышать MMM(3000)");
             }
         }
     }
